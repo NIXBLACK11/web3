402a43e1f5f7ee166a13b5ee18e1b61d6482e0bf
NIXBLACK11 <siddharthsinghrana11@gmail.com>
2025-02-22 15:05:30 +0530

    basic ownership

--- rust/readme.md
+++ rust/readme.md
@@ -14,3 +14,7 @@ Instructions:
 
 
 start from 1:13
+
+
+notes:
+https://petal-estimate-4e9.notion.site/Rust-for-Solana-contracts-1937dfd1073580c8a8fbc7e135e6d22a
--- rust/rust-week-26/src/main.rs
+++ rust/rust-week-26/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // data types, array, vectors
     println!("Hello, world!");
     let ans = sum(5, 10);
     println!("{}", ans);
@@ -9,6 +10,11 @@ fn main() {
     let arr = [1, 2, 3];
     println!("{}", arr.len());
 
+    // conditions and loops
+    loops_conditionals();
+
+    // Owner ship rules
+
     ownership_rules();
 
     let name = String::from("Siddharth");
@@ -18,9 +24,15 @@ fn main() {
     // println!("{}", name);
 
     // Possible solution
+    /*
     let (length, name) = get_len2(name);
     println!("{}", length);
     println!("{}", name);
+    */
+
+    // one good ear is to transfer omli tje read onli referense using &
+    println!("{}", get_len3(&name));
+
 }
 
 fn sum(a: u32, b: u32) -> u32 {
@@ -32,6 +44,19 @@ fn test_string() {
     println!("{}", name);
 }
 
+// Loops and conditionals
+fn loops_conditionals() {
+    let s = String::from("Test string ");
+    for ch in s.chars() {
+        if ch==' ' {
+            println!();
+        } else {
+            print!("{}", ch);
+        }
+    }
+}
+
+// Ownership
 // Rust says if the owner of a variable goes out of scope the value gets deleted, but if more than two point to the same value we get dangling pointers.
 fn ownership_rules() {
     let name = String::from("Siddharth Rana");
@@ -47,4 +72,8 @@ fn get_len(s: String) -> usize {
 
 fn get_len2(s: String) -> (usize, String) {
     return (s.len(), s);
+}
+
+fn get_len3(s: &String) -> usize {
+    return s.len();
 }
