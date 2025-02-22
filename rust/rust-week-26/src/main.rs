34c62a595cf917dd3e06b2d5a954364145f684e0
NIXBLACK11 <siddharthsinghrana11@gmail.com>
2025-02-22 15:25:42 +0530

    struct basics

--- rust/rust-week-26/src/main.rs
+++ rust/rust-week-26/src/main.rs
@@ -31,12 +31,15 @@ fn main() {
     */
 
     // one good ear is to transfer omli tje read onli referense using &
-    println!("{}", get_len3(&name));
+    // println!("{}", get_len3(&name));
 
+
+    //Struct
+    test2();
 }
 
 fn sum(a: u32, b: u32) -> u32 {
-    return a+b;
+    return a + b;
 }
 
 fn test_string() {
@@ -48,7 +51,7 @@ fn test_string() {
 fn loops_conditionals() {
     let s = String::from("Test string ");
     for ch in s.chars() {
-        if ch==' ' {
+        if ch == ' ' {
             println!();
         } else {
             print!("{}", ch);
@@ -76,4 +79,42 @@ fn get_len2(s: String) -> (usize, String) {
 
 fn get_len3(s: &String) -> usize {
     return s.len();
+}
+
+// Structs
+// struct User {
+//     name: String,
+//     active: bool,
+//     email: String,
+// }
+
+// fn use_struct() {
+//     let user1  = User{
+//         name: String::from("Siddharth"),
+//         active: false,
+//         email: "test".to_string()
+//     };
+    
+// }
+
+#[derive(Clone)]
+struct User {
+    active: bool,
+    sign_in_count: u64,
+    username: String,
+}
+
+fn test2() {
+    let user1 = User {
+        active: true,
+        sign_in_count: 1,
+        username: "harkirat".to_string()
+    };
+
+    change_name(user1.clone());
+    print!("User 1 username: {}", user1.active); // Error - can not use borrowed value
+}
+
+fn change_name(user1: User) {
+    print!("User 1 username: {:?}", user1.active);
 }
